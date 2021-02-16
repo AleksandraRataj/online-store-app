ffd1a6459cbdf4a51ca98755bbc440776da85d17
Aleksandra Rataj <aleksandrarataj2@gmail.com>
2021-02-16 10:56:03 +0100

    Add data file with products to project

---
+++ src/data.js
@@ -0,0 +1,66 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Sansewieria',
+            image: '/images/p1.jpg',
+            price: 50,
+            category: 'Sukulenty',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Bardzo prosta w uprawie roślina',
+        },
+        {
+            _id: '2',
+            name: 'Monstera Deliciosa',
+            image: '/images/p2.jpg',
+            price: 50,
+            category: 'Monstery',
+            rating: 5,
+            numReviews: 40,
+            description: 'Robi wrażenie!',
+        },
+        {
+            _id: '3',
+            name: 'Coffea Arabica',
+            image: '/images/p3.jpg',
+            price: 50,
+            category: 'Kawowce',
+            rating: 4,
+            numReviews: 13,
+            description: 'Miło zerknąć na nią pijąc kawę',
+        },
+        {
+            _id: '4',
+            name: 'Trzykrotka',
+            image: '/images/p4.jpg',
+            price: 50,
+            category: 'Sukulenty',
+            rating: 4.5,
+            numReviews: 16,
+            description: 'Przepiękne ubarwienie',
+        },
+        {
+            _id: '5',
+            name: 'Figowiec sprężysty',
+            image: '/images/p5.jpg',
+            price: 50,
+            category: 'Fikusy',
+            rating: 5,
+            numReviews: 17,
+            description: 'Ma duże, zdrowe liście',
+        },
+        {
+            _id: '6',
+            name: 'Strelicja',
+            image: '/images/p6.jpg',
+            price: 50,
+            category: 'Sukulenty',
+            rating: 4.5,
+            numReviews: 18,
+            description: 'Egzotyczny akcent w mieszkaniu',
+        },
+    ]
+}
+
+export default data;
