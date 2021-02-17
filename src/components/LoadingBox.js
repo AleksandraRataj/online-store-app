64f4c62f4781d3726567394fd9ef662c6fabd19e
Aleksandra Rataj <aleksandrarataj2@gmail.com>
2021-02-17 18:00:11 +0100

    Add LoadingBox and MessageBox components

---
+++ src/components/LoadingBox.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoadingBox = () => {
+    return(
+        <div>
+           <p><i className="fa fa-spinner fa-spin"/> Wczytywanie...</p>
+        </div>
+    );
+}
+
+export default LoadingBox;
