78d0daa33de94e36958f95821ad1c9cd747bda68
AleksandraRataj <57636297+AleksandraRataj@users.noreply.github.com>
2021-03-15 22:45:21 +0100

    Add src folder files

---
+++ frontend/src/actions/cartActions.js
@@ -0,0 +1,31 @@
+import Axios from "axios";
+import {CART_ADD_ITEM, CART_DELETE_ITEM} from "../constants/cartConstants";
+
+export const addToCart = (productID, quantity) => async (dispatch, getState) => {
+
+    const {data} = await Axios.get(`/api/products/${productID}`);
+    dispatch({
+        type: CART_ADD_ITEM,
+        payload: {
+            name: data.name,
+            image: data.image,
+            price: data.price,
+            countInStock: data.countInStock,
+            product: data._id,
+            quantity,
+        }
+    });
+
+    //po przeładowaniu strony zawartość koszyka pozostanie
+    localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
+}
+
+export const deleteFromCart = (productID) => async (dispatch, getState) => {
+
+    dispatch({
+        type: CART_DELETE_ITEM,
+        payload: productID
+    });
+
+    localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
+}
